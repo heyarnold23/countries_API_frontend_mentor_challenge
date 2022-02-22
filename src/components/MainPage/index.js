c0f4560ff1bbb7f0e8eb2385235497824a68bbf8
Arnold C <acabang23@gmail.com>
2022-02-21 18:05:35 -0800

    successfully fetching from countries api

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -1,7 +1,18 @@
-
+import React, { useEffect, useState } from 'react';
 
 
 export default function MainPage(){
+
+    const getApi = async () => {
+        const response = await fetch('https://restcountries.com/v2/all');
+        const data = await response.json()
+        console.log(data);
+
+    }
+
+    useEffect(() => {
+        getApi()
+    });
 
 
     return(
